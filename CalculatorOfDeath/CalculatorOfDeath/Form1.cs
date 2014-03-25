721ff1cc13711d89594c32bf798c22ee0eaa97d2
andrey <dred_95@mail.ru>
2014-03-25 14:22:24 +0700

    Linked interface, fabric and classes

--- CalculatorOfDeath/CalculatorOfDeath/Form1.cs
+++ CalculatorOfDeath/CalculatorOfDeath/Form1.cs
@@ -18,41 +18,41 @@ namespace CalculatorOfDeath
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+        private void transmitter(string name)
         {
             double FirstArgument = Convert.ToDouble(firstArgumentField.Text);
             double SecondArgument = Convert.ToDouble(secondArgumentField.Text);
-            Adder adder = new Adder();
-            result.Text = adder.Calculate(FirstArgument, SecondArgument);
+            IBinaryCalculator calculator = BinaryOperationFactory.Create(name);
+            result.Text = calculator.Calculate(FirstArgument, SecondArgument);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           transmitter("Adder");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            double FirstArgument = Convert.ToDouble(firstArgumentField.Text);
-            double SecondArgument = Convert.ToDouble(secondArgumentField.Text);
-            Subtraction subtraction = new Subtraction();
-            result.Text = subtraction.Calculate(FirstArgument, SecondArgument);
+            transmitter("Substraction");
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            double FirstArgument = Convert.ToDouble(firstArgumentField.Text);
-            double SecondArgument = Convert.ToDouble(secondArgumentField.Text);
-            Multiplication multiplication = new Multiplication();
-            result.Text = multiplication.Calculate(FirstArgument, SecondArgument);
+            transmitter("Multiplication");
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            double FirstArgument = Convert.ToDouble(firstArgumentField.Text);
-            double SecondArgument = Convert.ToDouble(secondArgumentField.Text);
-            Division division = new Division();
-            if (SecondArgument != 0.0f) //This is feature of calculatorOfDeath,Our calculator can divide by zero
+
+            transmitter("Division");
+           /* if (SecondArgument != 0.0f) //This is feature of calculatorOfDeath,Our calculator can divide by zero
                   result.Text = division.Calculate(FirstArgument, SecondArgument);
             else
             {
                 result.Text = "Мама ама криминал";
-            }
+            }*/
         }
     }
 }
