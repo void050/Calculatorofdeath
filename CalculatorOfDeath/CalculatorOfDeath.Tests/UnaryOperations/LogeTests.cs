8004ac9acb956dee63c1f7884750ef0847a61aef
Maxim Stupin <stupinmax@gmail.com>
2014-04-22 14:02:00 +0700

    Add some tests
    Log(e) and SquarePower

---
+++ CalculatorOfDeath/CalculatorOfDeath.Tests/UnaryOperations/LogeTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorOfDeath.UnaryOperations;
+using NUnit.Framework;
+
+namespace CalculatorOfDeath.Tests.UnaryOperations
+{
+    [TestFixture]
+    public class LogeTests
+    {
+        [Test]
+        public void LogeTest()
+        {
+            IUnaryCalculator calculator = UnaryOperationFactory.Create("Loge");
+            double result = calculator.Calculate(1);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ CalculatorOfDeath/CalculatorOfDeath.Tests/UnaryOperations/SquarePowerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorOfDeath.UnaryOperations;
+using NUnit.Framework;
+
+namespace CalculatorOfDeath.Tests.UnaryOperations
+{
+    [TestFixture]
+    public class SquarePowerTests
+    {
+        [Test]
+        public void SquarePowerTest()
+        {
+            IUnaryCalculator calculator = UnaryOperationFactory.Create("SquarePower");
+            double result = calculator.Calculate(8);
+            Assert.AreEqual(64, result);
+        }
+    }
+
+}
